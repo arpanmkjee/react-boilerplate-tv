5fe6a6d4e3ba74978765473ecd81b9ca7378a9f9
Arpan Mukharjee <arpan.m@Technovert.com>
2019-11-19 08:17:02 +0530

    init project

---
+++ app/containers/RepoListItem/IssueIcon.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import NormalIssueIcon from 'styled-components/IssueIcon';
+
+const IssueIcon = styled(NormalIssueIcon)`
+  fill: #ccc;
+  margin-right: 0.25em;
+`;
+
+export default IssueIcon;
